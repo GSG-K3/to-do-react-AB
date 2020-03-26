f0446945877f2dac66654e4b78604933f21a7e6a
bayanseder <bayansider88@gmail.com>
2020-03-26 16:00:36 +0200

    edit function #11

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 import "./ToDoList.css";
 
 let styles = { textDecoration: "line-through" };
-
 class ToDoList extends Component {
   state = {
     text: "hookah",
     items: []
   };
+  
+  
 
   setup = (e) => {
     this.setState({ text: e.target.value });
@@ -31,7 +32,10 @@ class ToDoList extends Component {
       items: this.state.items.concat(newItem),
       text: ""
     }));
-  };
+    localStorage.setItem("text",newItem.text);
+    localStorage.setItem("id" ,newItem.id)
+    localStorage.setItem("completed",newItem.completed)
+  }
   removeItem = (id) => {
     const remainder = this.state.items.filter((todo) => {
       if (todo.id !== id) return todo;
@@ -52,13 +56,27 @@ class ToDoList extends Component {
     this.setState({ items: updated });
     console.log("cccccc2", updated);
   };
+  editItem=(item)=>{
+    const inputF = document.getElementById('input');
+    inputF.value=item;
+    // const updatedTask= inputF.value;
+    // const change=()=>{
+    //      this.state.items.find((item)=>{
+    //       return  item.text  === updatedTask
+    //     })
+    //   }
+    
+    // const newArr= change()
+    // this.setState({items : newArr})
+
+  }
 
   render() {
     return (
       <div className="Table">
         <h1 className ="Header"> ToDo List</h1>
         <div className="InputDiv">
-        <input placeholder="Type here ...."
+        <input id="input" placeholder="Type here ...."
         className="InputField" 
         onChange={this.setup} 
         value={this.state.text} />
@@ -73,14 +91,15 @@ class ToDoList extends Component {
           items={this.state.items}
           removeItem={this.removeItem}
           handleChange={this.handleChange}
+          editItem={this.editItem}
         />
           </fieldset>
         </div>
       </div>
     );
   }
-}
 
+}
 class List extends Component {
   render() {
     return (
@@ -89,18 +108,22 @@ class List extends Component {
         <ul className="ListStyle">
           {this.props.items.map((item) => (
             <li className="ListItem" key={item.id} style={item.completed ? styles : null}>
-              {" "}
-              {item.text}
-              <div className="ButtonsDiv">
-              <button className="Removebtn" onClick={() => this.props.removeItem(item.id)}>
-                remove
-              </button>
+              <div className="first_li_div">
               <input
                 className="checkmark"
                 type="checkbox"
                 checked={item.completed}
                 onChange={() => this.props.handleChange(item.id)}
-              />
+                />
+                {"   "}
+              {item.text}
+              </div>
+              <div className="ButtonsDiv">
+              <button className="Removebtn" onClick={() => this.props.removeItem(item.id)}>
+                remove
+              </button>
+              <button className="Removebtn" onClick={()=>this.props.editItem(item.text)}>Edit</button>
+              
               </div>
             </li>
           ))}
